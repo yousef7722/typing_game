b2d2efe90f268901e5dc877788773fd25dbef9cd
yousef7722 <jonabilmc@gmail.com>
2023-03-08 03:07:15 +0200

    m

--- js/main.js
+++ js/main.js
@@ -9,12 +9,14 @@ let btn = document.querySelector(".btn")
 let word = document.querySelector(".word")
 let nextwords = document.querySelector(".nextwords")
 let input = document.querySelector("input[type='text']")
-
+let time = document.querySelector(".time")
+time.innerHTML=3
 
 btn.onclick = ()=>{
   welcome.style.display="none"
   btn.style.display="none";
   word.style.display="block";
+  time.style.display="block";
   let play =()=>{
   let random =Math.floor(Math.random()*data.length)
   word.innerHTML=`${data[random]}`
@@ -30,28 +32,35 @@ btn.onclick = ()=>{
     nextwords.appendChild(div)
   })
   }
+  let timing = setInterval(()=>{
+    time.innerHTML--
+  }, 1000)
   play()
   let gen =setInterval(()=>{
     if(input.value===word.innerHTML){
+      time.innerHTML=3
       console.log("nice")
       if(data.length >=1){
         input.value=""
         play()
       }else{
-        replay("Play again",gen)
+        replay("Play again",gen,timing)
       }
     }else{
-      replay("Try again",gen)
+      replay("Try again",gen,timing)
     }
   }, 3000)
 }
-let replay =(text,g)=>{
+let replay =(text,g,timing)=>{
   console.log(text)
   clearInterval(g)
+  clearInterval(timing)
   input.style.display="none";
+  time.style.display="none";
   nextwords.style.display="none";
   word.style.display="none";
   btn.style.display="block";
   btn.innerHTML= text;
+  time.innerHTML= 3;
   data=[...copy]
 }
